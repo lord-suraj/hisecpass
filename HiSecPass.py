87aceda1769e0a0589160da83588a4d7f119818c
lord-suraj <66644944+lord-suraj@users.noreply.github.com>
2020-06-09 01:52:50 +0530

    Update HiSecPass.py

--- HiSecPass.py
+++ HiSecPass.py
@@ -2,6 +2,108 @@
 
 # Author @lord_suraj
 
+print ("`````````````     ```                   ``    `````````                                             
+
+````````````                         ``.-:::////++:--..````                                         
+
+````````````                      `./sys/ss+//+++/:-....:/--.```````                                
+
+```````````                     `-syyyy/.--:/+/::----....:oyo--...-..``                             
+
+``````````                    `./o++sdmmh+::---....:+syyso//o+::+//--/:`                            
+
+``````````                   `:oo+yhNNMMMNdhhdhsosoosdNMNmds+++shhs++so-`                           
+
+..``  `````                `-/+ymmNMMMNMMMMMNNMMNNNNNNMMMMMNmysdmmmhhho-`                           
+
+..``` `````               .+yhdNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNmdNNdhs:.`                           
+
+..``````````             :hNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNmNmy/-``                            
+
+..```````````           `omNMMMMMMMMMMMMMMMMMMMMMNNNMMMMMMMMMMMMNms:.`                              
+
+.`````````````           :hmNMMMMMMMMMMMMMMMMMMMNNNMMMNNNNNNmdddmdys/`                              
+
+`````````````            `:oNMMMMMMMMMMMNNNmmmmmNNNNmmdhhhs+-.`.+ssds.                              
+
+`````````````              `/mMMMMMMMNNmddddhhhhhhhyyssoo/:.`` `-+yds`       `````                  
+
+```      `````              `+mMMMMMNdhhhhhyyysssssoo+++/:-.````-/sd+        ```````                
+
+`      ```````               `/NMMMNmysyyhhyssooooo+++/::-.``` ``/do`        ```..``                
+
+```````````````                oNMNhsooyyhhysoo++++///::---`     .d/         ``````                 
+
+``````````````                `.+mNs++ymNNNmdyso++///+++oss+:-.```ho .`     ````..`                 
+
+```````````                 `+os:-ddhddysooossydmddhdmdso+++++//+yos-+-     ````..`                 
+
+````````                    .dsss+yyy/`````````./mNNMh-````.--::-oyoo:`      ``...``                
+
+``````                      `oyhy+:oy```````````.dmhNy`````..--.-+.`o-        `....```              
+
+``                           :yhs/:+s.```````...sdo/+h+```......./  ` ````    `...``````           `
+
+`                            `/yh+-/oo.``.....:ydy+--:yo:.....-:/`  ` `.``    ``..````````````    ``
+
+```                           :oso:/+osoo++osyhhhy+:--:+so////:-`` ``....```  ```.....```...````````
+
+````                           ``.o//+++ooooooysoo+/::/:/+//:-....``.......`````......````......````
+
+``                                /o+++++++++oyydmdysmms-..:--....`.````..............`````........`
+
+`                                 .s++++++++osyhmNNmdmmy::o+..``...`````.--....```````````````....``
+
+`                                  -o++++++sdmNNmmdhyyyyhdNms.``.-.````.---...`````````````````...``
+
+`                                  `-oo+++oymNmdhhhyysooo++oo-.--.````..---...``````````````````````
+
+`                                   `:ooooooyyssyyyyyyys+:------.........--.......``````````````````
+
+`                       ```````     `-ossssssssoooosydyo+:::...`........-----..--....````````..`````
+
+``   ```````````````  ````.````    ``-osyyhyysyhyyyhdddhyo+-..``........---------..............`````
+
+``````.``````  ````` ``````````-//:.`-osssyyyyhdmmmmmmddddy/:-```.--:.`..--...------......```......`
+
+`......``````         ``````.:/:..-::/osssssssyhmNNNNNNNmds+:-``   ``--..................```........
+
+````````                `-/:.``.:oo+/+ssssssssoosssysssoo++/:-````   `:::--.``````````..............
+
+     ```      ````   `-/+ymo.-:+oooo+osssssssoooooooo++++++//-.` ````.///-:--.....```````...........
+
+                ```.-/yddmMNs++oooooossssssoooooooooo+++++++/:..`` `-+o+/::::::----......`..........
+
+             ``.-:/ossshmmMMNhsooooosssssssoooooooooooo+++++/:--...:ooo/:::::::::::::::--.......-...
+
+         ``..-/+ooooyhmmmmNNMMNhsoooosssssssoooooooooooo+++/:--:/+ooo+///::::::////////:---.........
+
+    ```.-:/+osssshmmdhhdmNNNMMMMNdyssooooossssssssssssso+//:-..:osso++////+++++++oo+++++//:---......
+
+  .--:+oydmdddmmdhdmNNNmmdmMMMMMMMMNdhyssssssssssssyyyyo/:-``.:+ssooooossyyyyyyyyyyyyyhhhhyso/:::-..
+
+ -soohmNNMMMMNNmmdhsssssyhmMMMMMMMMMMMNmmdhyyyssssyhhdhyo++oyhdhhhhhhhddddmdddmmmmNNNNNNNMNNmhoo+/-.
+
++dNNddNNMNNNNNNmmdddddhddNMMMMMMMMMMMMMMMMMMMNNNNNNNNMMMMMMNNNNNNMMMMMMMMNMNMMMMMMMMMMMMMMMMMmhso/:.
+
+NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNdysyo:
+
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMmmNNmMMMMMMMMMMMMMMMMMMNmhsdNs
+
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN/-/:.dMMMMMMMMMMMMMMMMMMNhsyMm
+
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMmy+ohNMMMMMMMMMMMMMMMMMMMdsdMM
+
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy::/ommMMMMMMMMMMMMMMMMMMNmMMM
+
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMo  `:hdmNMMMMMMMMMMMMMMMMMMMMM
+
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMo` .dmNNNMMMMMMMMMMMMMMMMMMMMN
+
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMs`.`hdNMMMMMMMMMMMMMMMMMMMMMMN
+
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMh```hmNMMMMMMMMMMMMMMMMMMMMMMM")
+
 import random
 
 from string import digits
