fa2ce32048678f922c1fb665587c24bdcb50f61a
lord-suraj <66644944+lord-suraj@users.noreply.github.com>
2020-06-09 01:57:05 +0530

    Update HiSecPass.py

--- HiSecPass.py
+++ HiSecPass.py
@@ -1,122 +1,12 @@
 # A Program To Create Highly Secure Passwords For Social Media Accounts.
-
 # Author @lord_suraj
 
-print ("`````````````     ```                   ``    `````````                                             
-
-````````````                         ``.-:::////++:--..````                                         
-
-````````````                      `./sys/ss+//+++/:-....:/--.```````                                
-
-```````````                     `-syyyy/.--:/+/::----....:oyo--...-..``                             
-
-``````````                    `./o++sdmmh+::---....:+syyso//o+::+//--/:`                            
-
-``````````                   `:oo+yhNNMMMNdhhdhsosoosdNMNmds+++shhs++so-`                           
-
-..``  `````                `-/+ymmNMMMNMMMMMNNMMNNNNNNMMMMMNmysdmmmhhho-`                           
-
-..``` `````               .+yhdNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNmdNNdhs:.`                           
-
-..``````````             :hNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNmNmy/-``                            
-
-..```````````           `omNMMMMMMMMMMMMMMMMMMMMMNNNMMMMMMMMMMMMNms:.`                              
-
-.`````````````           :hmNMMMMMMMMMMMMMMMMMMMNNNMMMNNNNNNmdddmdys/`                              
-
-`````````````            `:oNMMMMMMMMMMMNNNmmmmmNNNNmmdhhhs+-.`.+ssds.                              
-
-`````````````              `/mMMMMMMMNNmddddhhhhhhhyyssoo/:.`` `-+yds`       `````                  
-
-```      `````              `+mMMMMMNdhhhhhyyysssssoo+++/:-.````-/sd+        ```````                
-
-`      ```````               `/NMMMNmysyyhhyssooooo+++/::-.``` ``/do`        ```..``                
-
-```````````````                oNMNhsooyyhhysoo++++///::---`     .d/         ``````                 
-
-``````````````                `.+mNs++ymNNNmdyso++///+++oss+:-.```ho .`     ````..`                 
-
-```````````                 `+os:-ddhddysooossydmddhdmdso+++++//+yos-+-     ````..`                 
-
-````````                    .dsss+yyy/`````````./mNNMh-````.--::-oyoo:`      ``...``                
-
-``````                      `oyhy+:oy```````````.dmhNy`````..--.-+.`o-        `....```              
-
-``                           :yhs/:+s.```````...sdo/+h+```......./  ` ````    `...``````           `
-
-`                            `/yh+-/oo.``.....:ydy+--:yo:.....-:/`  ` `.``    ``..````````````    ``
-
-```                           :oso:/+osoo++osyhhhy+:--:+so////:-`` ``....```  ```.....```...````````
-
-````                           ``.o//+++ooooooysoo+/::/:/+//:-....``.......`````......````......````
-
-``                                /o+++++++++oyydmdysmms-..:--....`.````..............`````........`
-
-`                                 .s++++++++osyhmNNmdmmy::o+..``...`````.--....```````````````....``
-
-`                                  -o++++++sdmNNmmdhyyyyhdNms.``.-.````.---...`````````````````...``
-
-`                                  `-oo+++oymNmdhhhyysooo++oo-.--.````..---...``````````````````````
-
-`                                   `:ooooooyyssyyyyyyys+:------.........--.......``````````````````
-
-`                       ```````     `-ossssssssoooosydyo+:::...`........-----..--....````````..`````
-
-``   ```````````````  ````.````    ``-osyyhyysyhyyyhdddhyo+-..``........---------..............`````
-
-``````.``````  ````` ``````````-//:.`-osssyyyyhdmmmmmmddddy/:-```.--:.`..--...------......```......`
-
-`......``````         ``````.:/:..-::/osssssssyhmNNNNNNNmds+:-``   ``--..................```........
-
-````````                `-/:.``.:oo+/+ssssssssoosssysssoo++/:-````   `:::--.``````````..............
-
-     ```      ````   `-/+ymo.-:+oooo+osssssssoooooooo++++++//-.` ````.///-:--.....```````...........
-
-                ```.-/yddmMNs++oooooossssssoooooooooo+++++++/:..`` `-+o+/::::::----......`..........
-
-             ``.-:/ossshmmMMNhsooooosssssssoooooooooooo+++++/:--...:ooo/:::::::::::::::--.......-...
-
-         ``..-/+ooooyhmmmmNNMMNhsoooosssssssoooooooooooo+++/:--:/+ooo+///::::::////////:---.........
-
-    ```.-:/+osssshmmdhhdmNNNMMMMNdyssooooossssssssssssso+//:-..:osso++////+++++++oo+++++//:---......
-
-  .--:+oydmdddmmdhdmNNNmmdmMMMMMMMMNdhyssssssssssssyyyyo/:-``.:+ssooooossyyyyyyyyyyyyyhhhhyso/:::-..
-
- -soohmNNMMMMNNmmdhsssssyhmMMMMMMMMMMMNmmdhyyyssssyhhdhyo++oyhdhhhhhhhddddmdddmmmmNNNNNNNMNNmhoo+/-.
-
-+dNNddNNMNNNNNNmmdddddhddNMMMMMMMMMMMMMMMMMMMNNNNNNNNMMMMMMNNNNNNMMMMMMMMNMNMMMMMMMMMMMMMMMMMmhso/:.
-
-NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNdysyo:
-
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMmmNNmMMMMMMMMMMMMMMMMMMNmhsdNs
-
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN/-/:.dMMMMMMMMMMMMMMMMMMNhsyMm
-
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMmy+ohNMMMMMMMMMMMMMMMMMMMdsdMM
-
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy::/ommMMMMMMMMMMMMMMMMMMNmMMM
-
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMo  `:hdmNMMMMMMMMMMMMMMMMMMMMM
-
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMo` .dmNNNMMMMMMMMMMMMMMMMMMMMN
-
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMs`.`hdNMMMMMMMMMMMMMMMMMMMMMMN
-
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMh```hmNMMMMMMMMMMMMMMMMMMMMMMM")
-
 import random
-
 from string import digits
-
 from string import punctuation
-
 from string import ascii_letters
 
 symbols = ascii_letters + digits + punctuation
-
 secure_random = random.SystemRandom()
-
 password = "".join(secure_random.choice(symbols)for i in range(20))
-
-print ("Your Highly Secure Password Is :" , password)
-
+print ("Your Highly Secure Password Is : " , password)
